a23c1981fed96b5134eb2643a58a31d0cea46b9d
luizssilva99 <luizssilva_@outlook.com>
2023-08-22 15:21:50 -0300

    Initial commit

---
+++ JuntarPDF.py
@@ -0,0 +1,18 @@
+import PyPDF2
+import os
+
+# Instânciando o merge
+merger = PyPDF2.PdfMerger()
+
+# Listando os arquivos selecionados
+lista_arquivos = os.listdir("arquivos")
+lista_arquivos.sort()
+# print(lista_arquivos)
+
+# Percorrendo os arquivos da pasta
+for arquivo in lista_arquivos:
+    if ".pdf" in arquivo:
+        merger.append(f"arquivos/{arquivo}")
+
+# Gerando o PDF final
+merger.write("PDF Final.pdf")
